ff9b66c7bf1988ecda0125b0d3afd0b780555c98
Yash <yashkelkar09@gmail.com>
2022-07-21 22:22:52 -0400

    Second commit : Created test method of subtraction

---
+++ CMP-1005-Spring2002-Testing-master/CMP-1005-Spring2002-Testing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Calculator_Logic;
+
+namespace CMP_1005_Spring2002_Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            double x = calc.Add(1,1);
+
+            Console.WriteLine(x);
+        }
+    }
+}
